3a09ee6a34041cf5975673033207daf194cec5b0
samfass <sfasil374@gmail.com>
2022-06-13 11:55:15 -0700

    daynamic libraries

---
+++ 0x18-dynamic_libraries/src/1-strncat.c
@@ -0,0 +1,36 @@
+ History
+0 contributors
+33 lines (27 sloc) 554 Bytes
+#include "holberton.h"
+
+/**
+ * *_strncat - Concatenates two strings
+ * @dest: String of destiny, pointer to a char[]
+ * @src: Source string, pointer to a char[]
+ * @n: Integer parameter
+ * Return: Char[] concatenated
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *d;
+	char *s;
+	int counterS = 0, counterD = 0,  i, j;
+
+	for (s = src; *s != '\0'; s++)
+		counterS++;
+	for (d = dest; *d != '\0'; d++)
+		counterD++;
+
+	j = counterD;
+	if (n >= counterS)
+		n = counterS;
+	for (i  = 0; i < n; i++)
+	{
+		dest[j] = src[i];
+		j++;
+	}
+
+	return (dest);
+}
+
+
