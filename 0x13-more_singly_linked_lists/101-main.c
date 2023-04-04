3ca273fe7b47e98ab8700ab1aa1f7017cde78950
Fasil Solomon <98734803+samfass@users.noreply.github.com>
2023-04-04 13:18:49 +0300

    update

--- 0x13-more_singly_linked_lists/101-main.c
+++ 0x13-more_singly_linked_lists/101-main.c
@@ -1,47 +1,38 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
-size_t _free_listint_safe(listint_t **);
-
 /**
- * _add_node - Add a node at the beginning of a list
- *
- * @head: A pointer to the first element of a list
- * @n: The number to store in the new node
- *
- * Return: The address of the new node
- */
-listint_t *_add_node(listint_t **head, int n)
-{
-	listint_t *tmp;
-
-	tmp = malloc(sizeof(*tmp));
-	if (!tmp)
-		return (NULL);
-	tmp->n = n;
-	tmp->next = *head;
-	*head = tmp;
-	return (tmp);
-}
-
-/**
- * main - check the code .
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	listint_t *head;
-	listint_t *node;
-	size_t n;
+    listint_t *head;
+    listint_t *head2;
+    listint_t *node;
 
-	head = NULL;
-	node = _add_node(&head, 9);
-	node->next = _add_node(&head, 6);
-	n = print_listint_safe(head);
-	printf("%lu\n", n);
-	_free_listint_safe(&head);
-	return (0);
+    head2 = NULL;
+    add_nodeint(&head2, 0);
+    add_nodeint(&head2, 1);
+    add_nodeint(&head2, 2);
+    add_nodeint(&head2, 3);
+    add_nodeint(&head2, 4);
+    add_nodeint(&head2, 98);
+    add_nodeint(&head2, 402);
+    add_nodeint(&head2, 1024);
+    print_listint_safe(head2);
+    head = NULL;
+    node = add_nodeint(&head, 0);
+    add_nodeint(&head, 1);
+    add_nodeint(&head, 2);
+    add_nodeint(&head, 3);
+    add_nodeint(&head, 4);
+    node->next = add_nodeint(&head, 98);
+    add_nodeint(&head, 402);
+    add_nodeint(&head, 1024);
+    print_listint_safe(head);
+    return (0);
 }
